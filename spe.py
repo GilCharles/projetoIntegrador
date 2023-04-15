fc06f2140967ba1fdbe9b4336664c950235c8bf5
Gilvan Charles <gilvan.charles.silva@gmail.com>
2023-04-15 18:43:48 -0300

    testeeeeeee

--- spe.py
+++ spe.py
@@ -2,9 +2,9 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config.from_pyfile('config.py')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://host:senha@servidor/REGISTRO_ALUNOS_FAMERP'
 
-db = SQLAlchemy(app) 
+db = SQLAlchemy(app)
 
 from views import *
 
